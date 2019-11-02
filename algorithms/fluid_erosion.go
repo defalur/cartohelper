966dab4099e4b24da1cb878666f98569ff807099
Clément Rebut <clement.rebut@epita.fr>
2019-11-02 15:45:15 +0100

    Erosion: Implemented erosion and deposition of sediment

--- algorithms/fluid_erosion.go
+++ algorithms/fluid_erosion.go
@@ -4,6 +4,7 @@ import (
     "errors"
     "cartohelper/mapState"
     "math"
+    "fmt"
 )
 
 var startingWater float64 = 10.0
@@ -12,6 +13,11 @@ var deltaTime float64 = 0.1
 var A float64 = 1
 var g float64 = 9.81
 var l float64 = 1
+var lx float64 = 1
+var ly float64 = 1
+var Kc float64 = 5.0
+var Ks float64 = 5.0
+var Kd float64 = 5.0
 
 type tileData struct {
     totalFlow float64//total quantity of water that passed through the tile
@@ -42,7 +48,8 @@ func computeOutFlux(i, j int, mapData [][]tileData, h, w int) {
     //down
     neighbour, err = getTile(i + 1, j, h, w, mapData)
     if err == nil {
-        deltaHeight := mapData[i][j].height + mapData[i][j].water - neighbour.height - neighbour.water
+        deltaHeight := mapData[i][j].height + mapData[i][j].water - neighbour.height -
+                            neighbour.water
         mapData[i][j].outBot = computeFlow(mapData[i][j].outBot, deltaHeight)
     }
     //left
@@ -59,12 +66,119 @@ func computeOutFlux(i, j int, mapData [][]tileData, h, w int) {
     }
     //scale down if necessary
     n := mapData[i][j]
-    scale := math.Min(1, (n.water * l * l) /
+    scale := math.Min(1, (n.water * lx * ly) /
                 ((n,outTop + n.outBot + n.outLeft + n.outRight) * deltaTime))
     mapData[i][j].outTop *= scale
     mapData[i][j].outBot *= scale
     mapData[i][j].outLeft *= scale
     mapData[i][j].outRight *= scale
+}
+
+func updateWaterLvl(i, j int, mapData [][]tileData, h, w int) {
+    oldLevel := mapData[i][j].water
+    flowOut := mapData[i][j].outTop + mapData[i][j].outBot + mapData[i][j].outLeft +
+                    mapData[i][j].outRight
+    flowIn := 0.0
+    //top
+    var inTop float64
+    node, err := getTile(i - 1, j, h, w, mapData)
+    if err == nil {
+        flowIn += node.outBot
+        inTop = node.outBot
+    }
+    //bottom
+    var inBot float64
+    node, err := getTile(i + 1, j, h, w, mapData)
+    if err == nil {
+        flowIn += node.outTop
+        inBot = node.outTop
+    }
+    //left
+    var inLeft float64
+    node, err := getTile(i, j - 1, h, w, mapData)
+    if err == nil {
+        flowIn += node.outRight
+        inLeft = node.outRight
+    }
+    //right
+    var inRight float64
+    node, err := getTile(i, j + 1, h, w, mapData)
+    if err == nil {
+        flowIn += node.outLeft
+        inRight = node.outLeft
+    }
+    
+    //update water level
+    mapData[i][j].water += (deltatime * (flowIn - flowOut)) / (lx * ly)
+    
+    //compute velocity vector
+    levelAvg := (oldlevel + mapData[i][j].water) / 2
+    deltaWx := (inLeft - outLeft + outRight - inRight) / 2
+    deltaWy := (inTop - outTop + outBot - inBot) / 2
+    mapData[i][j].velX = deltaWx / (levelAvg * ly)
+    mapData[i][j].velY = deltaWy / (levelAvg * lx)
+}
+
+func computeGrad(a, b, d float64) float64 {
+    return (b - a) / (2 * d)
+}
+
+func tiltAngleSin(x0, x1, y0, y1 float64) float64 {
+    gradX := computeGrad(x0, x1, lx)
+    gradY := computeGrad(y0, y1, ly)
+    gradXsq := gradX * gradX
+    gradYsq := gradY * gradY
+    
+    sumSq := gradXsq + gradYsq
+    
+    return (math.Sqrt(sumSq)) / (math.Sqrt(1 + sumSq))
+}
+
+func mag(x, y float64) float64 {
+    return math.Sqrt(x * x + y * y)
+}
+
+func computeErosion(i, j int, mapData [][]tileData, h, w int) {
+    //up
+    up, err := getTile(i - 1, j, h, w, mapData)
+    upHeight := mapData[i][j].height
+    if err == nil {
+        upHeight = up.height
+    }
+    //bottom
+    down, err := getTile(i + 1, j, h, w, mapData)
+    downHeight := mapData[i][j].height
+    if err == nil {
+        downHeight = down.height
+    }
+    //left
+    left, err := getTile(i, j - 1, h, w, mapData)
+    leftHeight := mapData[i][j].height
+    if err == nil {
+        leftHeight = left.height
+    }
+    //right
+    right, err := getTile(i, j + 1, h, w, mapData)
+    rightHeight := mapData[i][j].height
+    if err == nil {
+        rightHeight = right.height
+    }
+    
+    //compute tilt angle
+    n := mapData[i][j]
+    //Compute capacity
+    C := Kc * tiltAngleSin(leftHeight, rightHeight, upHeight, downHeight) * mag(n.velx, n.vely)
+    //update terrain height and suspended soil
+    curSediment := n.sediment
+    if C > curSediment {
+        dif := C - curSediment
+        mapData[i][j].height = mapData[i][j].height - Ks * dif
+        mapData[i][j].sediment = mapData[i][j].sediment + Ks * dif
+    } else {
+        dif := curSediment - C
+        mapData[i][j].height = mapData[i][j].height + Kd * dif
+        mapData[i][j].sediment = mapData[i][j].sediment - Kd * dif
+    }
 }
 
 //============== Utility functions ======================
@@ -115,10 +229,13 @@ func FluidErosion(mapState mapstate.MapState) {
     //compute output flux for all tiles
         waterAmount = iterate(mapData, mapState.GetHeight(), mapState.GetWidth(), computeOutFlux)
     //update water level on all tiles
-    //compute velocity vector on all tiles
+    //and compute velocity vector on all tiles
+        iterate(mapData, mapState.GetHeight(), mapState.GetWidth(), updateWaterLvl)
     //compute sediment deposition and erosion
+        iterate(mapData, mapState.GetHeight(), mapState.GetWidth(), computeErosion)
     //compute sediment transportation from velicoty vector
     //update water level with evaporation
+        fmt.Println("Water: ", waterAmount)
     //}end loop
     }
 }
